258f5dffda3cd9b34d6b49a59cd403a3646e26fb
Audionut <audionut11@gmail.com>
2024-05-09 22:54:24 +1000

    Update - TIK categories

--- ptp-add-filter-all-releases-anut.js
+++ ptp-add-filter-all-releases-anut.js
@@ -1257,7 +1257,7 @@
                 api_query_url =
                     "https://cinematik.net/api/torrents/filter?imdbId=" +
                     imdb_id.split("tt")[1] +
-                    "&categories[0]=1&categories[1]=3&categories[2]=5&categories[3]=6&api_token=" +
+                    "&categories[0]=1&categories[1]=2&categories[2]=3&categories[3]=4&categories[4]=5&categories[5]=6&api_token=" +
                     TIK_API_TOKEN;
             }
             else if (tracker === "Aither") {
