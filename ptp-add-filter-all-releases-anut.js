796472e942ee32e32dfa2a0a6aa684588e1de983
Audionut <audionut11@gmail.com>
2024-05-21 00:29:41 +1000

    Minor fix

--- ptp-add-filter-all-releases-anut.js
+++ ptp-add-filter-all-releases-anut.js
@@ -2167,7 +2167,7 @@
             let cln = line_example.cloneNode(true);
 
             if (improved_tags) {
-                cln.querySelector(".torrent-info-link").textContent = `[${torrent.site}] ` + get_simplified_title(torrent.info_text);
+                cln.querySelector(".torrent-info-link").textContent = `[${torrent.site}] / ` + get_simplified_title(torrent.info_text);
             } else if (show_tracker_name) {
                 cln.querySelector(".torrent-info-link").textContent = `[${torrent.site}] ` + torrent.info_text;
             } else {
