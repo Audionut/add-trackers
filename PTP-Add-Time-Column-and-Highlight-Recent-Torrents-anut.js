6a07d77c27684833ea1d78553f35694e6ca8e9c0
Audionut <audionut11@gmail.com>
2024-06-14 09:48:41 +1000

    revert

--- PTP-Add-Time-Column-and-Highlight-Recent-Torrents-anut.js
+++ PTP-Add-Time-Column-and-Highlight-Recent-Torrents-anut.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PTP - Add Time Column and Highlight Recent Torrents
 // @namespace    PTP-Add-Time-Column-and-Highlight-Recent-Torrents
-// @version      0.5.4
+// @version      0.5.5
 // @description  Add a Time column to the Torrent Group Page, Collage Page,
 //               Artist Page, and Bookmark Page.
 //               Highlight recent and latest torrent within a group.
@@ -44,26 +44,23 @@
     }
 
     function torrentsGroupPage() {
-        const torrentTableHead = $('.torrent_table thead tr th');
-        if (!torrentTableHead.filter(':contains("Time")').length) {
-            torrentTableHead.eq(0).after($('<th>Time</th>'));
+        if (!$('.torrent_table thead tr th:contains("Time")').length) {
+            $('.torrent_table thead tr th:nth(0)').after($('<th>Time</th>'));
         }
-        const torrentEditions = $('.group_torrent td.basic-movie-list__torrent-edition');
-        const torrentInfos = $('.torrent_info_row td');
-        torrentEditions.add(torrentInfos).each((i, element) => {
-            const el = $(element);
-            const colspan = parseInt(el.attr('colspan'));
-            el.attr('colspan', colspan + 1);
+        $('.group_torrent td.basic-movie-list__torrent-edition').each((i, edition) => {
+            $(edition).attr('colspan', parseInt($(edition).attr('colspan')) + 1);
         });
-    
+        $('.torrent_info_row td').each((i, info) => {
+            $(info).attr('colspan', parseInt($(info).attr('colspan')) + 1);
+        });
+
         handleExistingTorrents();
         handleNewTorrents();
     }
-    
+
     function handleExistingTorrents() {
         let times = [];
-        const torrentHeaders = $('.group_torrent_header');
-        torrentHeaders.each(function(i, element) {
+        $('.group_torrent_header').each(function(i, element) {
             const torrentRow = $(element);
             if (!torrentRow.find('td.time-cell').length) {
                 let time = torrentRow.next().find('span.time').first();
@@ -74,8 +71,8 @@
                         const formattedTime = formatTime(moment.unix(unixTimestamp));
                         time.html(formattedTime);
                         times.push(timeTitle);
-    
-                        time.addClass('nobr');
+
+                        $(time).addClass('nobr');
                         const timeCell = $('<td class="time-cell"></td>').append(time);
                         torrentRow.find('td:nth(0)').after(timeCell);
                     }
@@ -84,11 +81,10 @@
         });
         highlightTimes(times);
     }
-    
+
     function handleNewTorrents() {
         let times = [];
-        const torrentHeaders = $('.group_torrent_header');
-        torrentHeaders.each(function(i, element) {
+        $('.group_torrent_header').each(function(i, element) {
             const torrentRow = $(element);
             if (!torrentRow.find('td.time-cell').length) {
                 let time = torrentRow.find('span.release.time').first();
@@ -97,8 +93,15 @@
                     if (!isNaN(unixTimestamp)) {
                         const formattedTime = formatTime(moment.unix(unixTimestamp));
                         time.html(formattedTime);
-                        times.push(unixTimestamp);
+                        times.push(time.attr('title'));
+
+                        $(time).addClass('nobr');
+                        const timeCell = $('<td class="time-cell"></td>').append(time);
+                        torrentRow.find('td:nth(0)').after(timeCell);
                     }
+                } else {
+                    const timeCell = $('<td class="time-cell"></td>').append($('<span>').addClass('nobr'));
+                    torrentRow.find('td:nth(0)').after(timeCell);
                 }
             }
         });
