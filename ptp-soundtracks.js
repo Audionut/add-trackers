ce178a271158d088a5ad1f319ea57f1cfd58b0e9
Audionut <audionut11@gmail.com>
2024-08-04 14:55:13 +1000

    Update - soundtrack artists

--- ptp-soundtracks.js
+++ ptp-soundtracks.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PTP - Add iMDB Soundtracks
 // @namespace    https://github.com/Audionut/add-trackers
-// @version      0.9
+// @version      1.0
 // @description  Add soundtracks from IMDB API
 // @author       passthepopcorn_cc (mods by Audionut)
 // @match        https://passthepopcorn.me/torrents.php?id=*
@@ -9,27 +9,54 @@
 // @connect      api.graphql.imdb.com
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
-    let imdb_id;
-
-    try {
-        imdb_id = "tt" + document.getElementById("imdb-title-link").href.split("/tt")[1].split("/")[0];
-    } catch(e) { // replaced by ratings box script...
-        imdb_id = "tt" + [...document.querySelectorAll(".rating")].find(a => a.href.includes("imdb.com")).href.split("/tt")[1].split("/")[0];
-    }
-
-    const insertAfter = (newNode, referenceNode) => {
-        referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-    }
-
-    const fetchTrackData = async (imdb_id) => {
+    const fetchNames = async (uniqueIds) => {
         const url = `https://api.graphql.imdb.com/`;
         const query = {
             query: `
                 query {
-                    title(id: "${imdb_id}") {
+                    names(ids: ${JSON.stringify(uniqueIds)}) {
+                        id
+                        nameText {
+                            text
+                        }
+                    }
+                }
+            `
+        };
+
+        return new Promise((resolve, reject) => {
+            GM_xmlhttpRequest({
+                method: "POST",
+                url: url,
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                data: JSON.stringify(query),
+                onload: function (response) {
+                    if (response.status >= 200 && response.status < 300) {
+                        const data = JSON.parse(response.responseText);
+                        console.log("Name ID query output:", data.data.names); // Log the output
+                        resolve(data.data.names);
+                    } else {
+                        reject("Failed to fetch names");
+                    }
+                },
+                onerror: function (response) {
+                    reject("Request error");
+                }
+            });
+        });
+    };
+
+    const fetchSoundtrackData = async (titleId) => {
+        const url = `https://api.graphql.imdb.com/`;
+        const query = {
+            query: `
+                query {
+                    title(id: "${titleId}") {
                         soundtrack(first: 30) {
                             edges {
                                 node {
@@ -62,140 +89,118 @@
             `
         };
 
-        return new Promise((resolve, reject) => {
-            GM_xmlhttpRequest({
-                method: "POST",
-                url: url,
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                data: JSON.stringify(query),
-                onload: function(response) {
-                    if (response.status >= 200 && response.status < 300) {
-                        const data = JSON.parse(response.responseText);
-                        const tracks = data.data.title.soundtrack.edges.map(edge => {
-                            const node = edge.node;
-                            const trackInfo = {
-                                title: node.text,
-                                artistIds: extractArtistsFromComments(node.comments),
-                                writers: extractWritersFromComments(node.comments)
-                            };
-                            return trackInfo;
+        GM_xmlhttpRequest({
+            method: "POST",
+            url: url,
+            headers: {
+                "Content-Type": "application/json"
+            },
+            data: JSON.stringify(query),
+            onload: async function (response) {
+                if (response.status >= 200 && response.status < 300) {
+                    const data = JSON.parse(response.responseText);
+                    const soundtracks = data.data.title.soundtrack.edges;
+
+                    // Extract unique IDs from comments
+                    const uniqueIds = [];
+                    soundtracks.forEach(edge => {
+                        edge.node.comments.forEach(comment => {
+                            const match = comment.markdown.match(/\[link=nm(\d+)\]/);
+                            if (match) {
+                                uniqueIds.push(`nm${match[1]}`);
+                            }
                         });
-                        fetchArtistNames(tracks).then(tracksWithArtists => {
-                            appendSongs(tracksWithArtists);
+                    });
+
+                    const uniqueIdSet = [...new Set(uniqueIds)];
+                    const names = await fetchNames(uniqueIdSet);
+
+                    // Map IDs to names
+                    const idToNameMap = {};
+                    names.forEach(name => {
+                        idToNameMap[name.id] = name.nameText.text;
+                    });
+                    console.log("ID to Name Map:", idToNameMap); // Log the ID to Name mapping
+
+                    // Process the soundtrack data
+                    const processedSoundtracks = soundtracks.map(edge => {
+                        const soundtrack = edge.node;
+                        let artistName = null;
+                        let artistLink = null;
+                        let artistId = null;
+
+                        // Try to find "Performed by" first
+                        soundtrack.comments.forEach(comment => {
+                            const performedByMatch = comment.markdown.match(/Performed by \[link=nm(\d+)\]/);
+                            if (performedByMatch && !artistName) {
+                                artistId = `nm${performedByMatch[1]}`;
+                                artistName = idToNameMap[artistId];
+                                artistLink = `https://www.imdb.com/name/${artistId}/`;
+                                console.log(`Matched "Performed by" ID: ${artistId}, Artist Name: ${artistName}, Artist Link: ${artistLink}`);
+                            }
                         });
-                    } else {
-                        console.error("Failed to fetch track data", response);
-                        reject(response);
-                    }
-                },
-                onerror: function(response) {
-                    console.error("Request error", response);
-                    reject(response);
+
+                        // If no "Performed by" found, try to find other roles
+                        if (!artistName) {
+                            soundtrack.comments.forEach(comment => {
+                                const match = comment.markdown.match(/\[link=nm(\d+)\]/);
+                                if (match && !artistName) {
+                                    artistId = `nm${match[1]}`;
+                                    artistName = idToNameMap[artistId] || match[0];
+                                    artistLink = `https://www.imdb.com/name/${artistId}/`;
+                                    console.log(`Matched other role ID: ${artistId}, Artist Name: ${artistName}, Artist Link: ${artistLink}`);
+                                } else if (!match && !artistName) {
+                                    const performedByMatch = comment.markdown.match(/Performed by (.*)/);
+                                    if (performedByMatch) {
+                                        artistName = performedByMatch[1];
+                                        artistLink = `https://www.imdb.com/find/?q=${encodeURIComponent(artistName)}`;
+                                        console.log(`Performed by match: Artist Name: ${artistName}, Artist Link: ${artistLink}`); // Log performed by match
+                                    }
+                                }
+                            });
+                        }
+
+                        // Fallback to amazonMusicProducts if no artist name found
+                        if (!artistName && soundtrack.amazonMusicProducts.length > 0) {
+                            const product = soundtrack.amazonMusicProducts[0];
+                            if (product.artists && product.artists.length > 0) {
+                                artistName = product.artists[0].artistName.text;
+                                artistLink = `https://www.imdb.com/find/?q=${encodeURIComponent(artistName)}`;
+                                console.log(`Amazon Music Product Artist: Artist Name: ${artistName}, Artist Link: ${artistLink}`); // Log amazon music product artist
+                            } else {
+                                artistName = product.productTitle.text;
+                                artistLink = `https://www.imdb.com/find/?q=${encodeURIComponent(artistName)}`;
+                                console.log(`Amazon Music Product Title: Artist Name: ${artistName}, Artist Link: ${artistLink}`); // Log amazon music product title
+                            }
+                        }
+
+                        // Final fallback to text
+                        if (!artistName) {
+                            artistName = soundtrack.text;
+                            artistLink = `https://www.imdb.com/find/?q=${encodeURIComponent(artistName)}`;
+                            console.log(`Fallback to soundtrack text: Artist Name: ${artistName}, Artist Link: ${artistLink}`); // Log fallback to soundtrack text
+                        }
+
+                        return {
+                            title: soundtrack.text,
+                            artist: artistName,
+                            link: artistLink,
+                            artistId: artistId
+                        };
+                    });
+
+                    appendSongs(processedSoundtracks, idToNameMap);
+                } else {
+                    console.error("Failed to fetch soundtrack data", response);
                 }
-            });
-        });
-    };
-
-    const extractArtistsFromComments = (comments) => {
-        const artistRegex = /Performed by \[link=nm(\d+)\]/;
-        const artistIds = comments
-            .map(comment => {
-                const match = comment.markdown.match(artistRegex);
-                return match ? `nm${match[1]}` : null;
-            })
-            .filter(Boolean);
-
-        return artistIds;
-    };
-
-    const extractWritersFromComments = (comments) => {
-        const writerRegex = /Written by \[link=nm(\d+)\]/g;
-        const writerIds = [];
-        comments.forEach(comment => {
-            let match;
-            while ((match = writerRegex.exec(comment.markdown)) !== null) {
-                writerIds.push(`nm${match[1]}`);
+            },
+            onerror: function (response) {
+                console.error("Request error", response);
             }
         });
-
-        return writerIds;
     };
 
-    const fetchArtistNames = (tracks) => {
-        const artistIds = tracks.flatMap(track => track.artistIds);
-        const writerIds = tracks.flatMap(track => track.writers);
-        const uniqueIds = [...new Set([...artistIds, ...writerIds])];
-
-        if (uniqueIds.length === 0) {
-            console.log("No artist or writer IDs found.");
-            return Promise.resolve(tracks);
-        }
-
-        const url = `https://api.graphql.imdb.com/`;
-        const query = {
-            query: `
-                query {
-                    names(ids: ${JSON.stringify(uniqueIds)}) {
-                        id
-                        nameText {
-                            text
-                        }
-                    }
-                }
-            `
-        };
-
-        return new Promise((resolve, reject) => {
-            GM_xmlhttpRequest({
-                method: "POST",
-                url: url,
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                data: JSON.stringify(query),
-                onload: function(response) {
-                    if (response.status >= 200 && response.status < 300) {
-                        const data = JSON.parse(response.responseText);
-                        const nameMap = data.data.names.reduce((acc, name) => {
-                            acc[name.id] = name.nameText.text;
-                            return acc;
-                        }, {});
-
-                        const tracksWithArtists = tracks.map(track => {
-                            const artists = track.artistIds.map(id => ({
-                                id,
-                                name: nameMap[id],
-                                link: `https://www.imdb.com/name/${id}`
-                            })).filter(artist => artist.name); // filter out undefined artist names
-                            const writers = track.writers.map(id => ({
-                                id,
-                                name: nameMap[id],
-                                link: `https://www.imdb.com/name/${id}`
-                            })).filter(writer => writer.name); // filter out undefined writer names
-
-                            return {
-                                ...track,
-                                artists: artists.length > 0 ? artists : writers
-                            };
-                        });
-
-                        resolve(tracksWithArtists);
-                    } else {
-                        console.error("Failed to fetch names", response);
-                        reject(response);
-                    }
-                },
-                onerror: function(response) {
-                    console.error("Request error", response);
-                    reject(response);
-                }
-            });
-        });
-    };
-
-    const appendSongs = (songs) => {
+    const appendSongs = (songs, idToNameMap) => {
         let movie_title = document.querySelector(".page__title").textContent.split("[")[0].trim();
         if (movie_title.includes(" AKA ")) movie_title = movie_title.split(" AKA ")[1]; // 0 = title in foreign lang, 1 = title in eng lang
 
@@ -205,12 +210,12 @@
         let border_color = window.getComputedStyle(cast_container.querySelector("tbody > tr > td"), null).getPropertyValue("border-color").split("none")[0];
 
         let new_panel = document.createElement("div");
-        new_panel.id = "imdb-soundtrack"
+        new_panel.id = "imdb-soundtrack";
         new_panel.className = "panel";
         new_panel.style.padding = 0;
         new_panel.style.margin = "18px 0";
 
-        new_panel.innerHTML = '<div class="panel__heading"><span class="panel__heading__title"><a href="https://www.imdb.com/title/' + imdb_id + '/soundtrack"><span style="color: rgb(242, 219, 131);">IMDb</span> Soundtrack</a></span></div>';
+        new_panel.innerHTML = '<div class="panel__heading"><span class="panel__heading__title"><a href="https://www.imdb.com/title/' + imdbId + '/soundtrack"><span style="color: rgb(242, 219, 131);">iMDB</span> Soundtrack</a></span></div>';
 
         new_panel.querySelector(".panel__heading").style.display = "flex";
         new_panel.querySelector(".panel__heading").style.justifyContent = "space-between";
@@ -255,13 +260,13 @@
 
         let j = 0;
         for (let i = 0; i < songs.length / 2; i++) {
-            let div = createSongDiv(songs[i], movie_title, j, bg_color_1, bg_color_2);
+            let div = createSongDiv(songs[i], movie_title, j, bg_color_1, bg_color_2, idToNameMap);
             songs_col_1.appendChild(div);
             j++;
         }
 
         for (let i = Math.ceil(songs.length / 2); i < songs.length; i++) {
-            let div = createSongDiv(songs[i], movie_title, j, bg_color_1, bg_color_2);
+            let div = createSongDiv(songs[i], movie_title, j, bg_color_1, bg_color_2, idToNameMap);
             songs_col_2.appendChild(div);
             j++;
         }
@@ -273,20 +278,15 @@
         cast_container.parentNode.insertBefore(new_panel, cast_container);
     };
 
-    const createSongDiv = (song, movie_title, index, bg_color_1, bg_color_2) => {
+    const createSongDiv = (song, movie_title, index, bg_color_1, bg_color_2, idToNameMap) => {
         let div = document.createElement("div");
         div.style.height = "31px";
         div.style.overflow = "hidden";
         div.style.padding = "6px 14px";
 
-        let title_text = song.title;
-        let artist = song.artists.length > 0 ? song.artists[0] : { name: "Unknown Artist", link: "#" };
-        let artist_text = artist.name;
-
         let track_line = document.createElement("a");
-        track_line.textContent = title_text;
+        track_line.textContent = song.title;
         track_line.title = song.title;
-        //track_line.style.verticalAlign = "middle";
         track_line.href = "https://www.youtube.com/results?search_query=" + movie_title.replace(/&/, " and ") + " " + song.title.replace(/&/, " and ");
         track_line.target = "_blank";
 
@@ -294,8 +294,8 @@
         seperator.innerHTML = "-&nbsp;&nbsp;&nbsp;";
 
         let artist_link = document.createElement("a");
-        artist_link.textContent = artist_text;
-        artist_link.href = artist.link;
+        artist_link.textContent = song.artistId ? idToNameMap[song.artistId] : song.artist;
+        artist_link.href = song.link;
         artist_link.target = "_blank";
         artist_link.style.marginRight = "10px";
 
@@ -309,5 +309,8 @@
         return div;
     };
 
-    fetchTrackData(imdb_id);
+    const imdbUrl = document.querySelector("a#imdb-title-link.rating").getAttribute("href");
+    const imdbId = imdbUrl.split("/")[4];
+
+    fetchSoundtrackData(imdbId);
 })();
